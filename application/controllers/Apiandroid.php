9ff7cf0ff6e89bc61614150005841b9daa460d69
Maran <maran.happysanz@gmail.com>
2020-09-28 18:59:58 +0530

    api changes

--- application/controllers/Apiandroid.php
+++ application/controllers/Apiandroid.php
@@ -12,7 +12,7 @@ class Apiandroid extends CI_Controller {
         parent::__construct();
 		$this->load->model("apiandroidmodel");
 		$this->load->helper("url");
-		$this->load->library('session');
+		//$this->load->library('session');
     }
 
 	public function checkMethod()
--- application/controllers/Apiios.php
+++ application/controllers/Apiios.php
@@ -12,7 +12,7 @@ class Apiios extends CI_Controller {
         parent::__construct();
 		$this->load->model("apiiosmodel");
 		$this->load->helper("url");
-		$this->load->library('session');
+		//$this->load->library('session');
     }
 
 	public function checkMethod()
