212e2a43baf6eb7506ba58850d669c017b87bbfc
kamal <>
2020-09-30 09:06:05 +0530

    Login committed

--- console/resources/views/login.blade.php
+++ console/resources/views/login.blade.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>Login Page</title>
-		<meta name="description" content="Login page example" />
+		<meta name="description"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<!--begin::Fonts-->
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
