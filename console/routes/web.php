95706979cb9ac3bebf9f3c61d10a4dbe0c6e26c6
kamal <>
2020-09-30 08:43:41 +0530

    Route updated

--- console/routes/web.php
+++ console/routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/','App\Http\Controllers\Homecontroller@login');
+
 Route::get('/admin/login','App\Http\Controllers\Homecontroller@login');
 
 Route::get('logout','App\Http\Controllers\Homecontroller@logout');
