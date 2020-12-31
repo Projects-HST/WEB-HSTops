0ffb172f0832b1afdf6e077a79aa3e41d18e997e
Maran <maran.happysanz@gmail.com>
2020-12-31 13:19:52 +0530

    home update

--- application/views/tamil/home.php
+++ application/views/tamil/home.php
@@ -35,9 +35,10 @@
         </div>
         <div class="banner3-social">
             <ul>
-                <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="https://www.facebook.com/OfficeOfOPS/"><i class="fa fa-facebook-f"></i></a></li>
+                <li><a href="https://twitter.com/OfficeOfOPS/"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="https://www.instagram.com/officeof_ops/"><i class="fa fa-instagram"></i></a></li>
+				<li><a href="https://www.linkedin.com/in/panneerselvam-o-033762192/"><i class="fa fa-linkedin"></i></a></li>
             </ul>
         </div>
         <!-- video button -->
