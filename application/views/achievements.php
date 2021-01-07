5814f78d51217dec8d57b2ce4376aaca3b232244
Maran <maran.happysanz@gmail.com>
2021-01-07 13:56:35 +0530

    Update achievements.php

--- application/views/achievements.php
+++ application/views/achievements.php
@@ -46,7 +46,7 @@
                         <div class="thumb">
 							<img src="<?php echo $assets_url.$rows['achievement_image'];?>" alt="">
                         </div>
-                        <div class="content" style="min-height:400px;">
+                        <div class="content" style="min-height:200px;">
 							<h4 class="title"><a href="blog-details.html"><?php echo $rows['achievement_title_en'];?></a></h4>
                              <ul class="blog-meta d-flex flex-column flex-lg-row">
                                 <li><a href="#">05 December 2019</a></li>
